ae17b4a5572d70f1131869aa95476a2b250397b2
IRjSI <rajusuthar0077@gmail.com>
2025-04-30 11:32:56 +0530

    refactor: improve user registration and challenge management routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,35 +1,43 @@
 import UserModel from "../models/user.model.js";
 
 const userRegister = async (req,res) => {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-        return res.status(400).json(400, {
-            message: "All fields required",
+    try {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(400).json(400, {
+                message: "All fields required",
+                success: false
+            })
+        }
+        
+        const existingUser = await UserModel.findOne({ username });
+        if (existingUser) {
+            console.log(existingUser)
+            return res.status(400).json({
+                message: "User already exists",
+                success: false
+            })
+        }
+        
+        const newUser = await UserModel.create({
+            username,
+            email,
+            password
+        })
+        
+        if (newUser) {
+            return res.status(201).json({
+                data: newUser,
+                message: "User created",
+                success: true
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
             success: false
-        })
-    }
-    
-    const existingUser = await UserModel.findOne({ username });
-    if (existingUser) {
-        console.log(existingUser)
-        return res.status(400).json({
-            message: "User already exists",
-            success: false
-        })
-    }
-    
-    const newUser = await UserModel.create({
-        username,
-        email,
-        password
-    })
-    
-    if (newUser) {
-        return res.status(201).json({
-            data: newUser,
-            message: "User created",
-            success: true
-        })
+        });
     }
 }
 
@@ -57,7 +65,11 @@ const userLogin = async (req,res) => {
             success: true
         })
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });
     }
 }
 
@@ -71,9 +83,12 @@ const addChallenges = async (req,res) => {
             })
         }
 
-        const updatedUser = await UserModel.findOneAndUpdate({ _id: req.userId }, {
-            challenges: challenge
-        });
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            req.userId,
+            { $push: { challenges: challenge } },
+            { new: true }
+        );
+        
         if (!updatedUser) {
             return res.status(400).json({
                 message: "error adding challenge",
@@ -81,13 +96,17 @@ const addChallenges = async (req,res) => {
             })
         }
 
-        return res.status(400).json({
+        return res.status(200).json({
             data: updatedUser,
             message: "Challenge added",
-            success: false
+            success: true
         })
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });
     }
 }
 
@@ -108,10 +127,17 @@ const getNumberOfChallenges = async (req,res) => {
         })
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });
     }
 }
 
 export {
     userRegister,
-    userLogin
+    userLogin,
+    addChallenges,
+    getNumberOfChallenges,
+
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { userLogin, userRegister } from "../controllers/user.controller.js";
+import { addChallenges, getNumberOfChallenges, userLogin, userRegister } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post('/register', userRegister);
 userRouter.post('/login', userLogin);
-userRouter.post('/challenges', userLogin);
+userRouter.post('/add-challenge', addChallenges);
+userRouter.get('/get-challenges', getNumberOfChallenges);
 
 export default userRouter;
