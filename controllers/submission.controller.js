70c03fc7240fcb13e133215195c1f5b0d8f0993a
IRjSI <rajusuthar0077@gmail.com>
2025-09-10 20:09:21 +0530

    fix: correct spelling of 'received' in console log statement

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,6 +1,7 @@
 import { enqueueSolution } from "../utils/queue.js";
 
 const checkSolution = async (req, res) => {
+  console.log("recieved")
   const { iframeDoc } = req.body;
   const solutionId = `solution:${Date.now()}`;
 
